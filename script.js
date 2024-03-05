c293d5cd9151f5ea586567fddeb3ef92734261bd
Abir Chodha <abirchodha1@gmail.com>
2024-03-06 02:51:53 +0530

    Add score functionality

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -23,4 +25,10 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, swicth to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+  } else {
+    // Switch to next player
+  }
 });
